2e4ca342f7251be0eaa2c62607235e974e2450f3
ZebaFarhangi <ziba2338@gmail.com>
2024-09-12 07:26:00 +1000

    trying to make the btn work

--- app/src/main/java/com/example/androidappdev/login/LoginPageFragment.kt
+++ app/src/main/java/com/example/androidappdev/login/LoginPageFragment.kt
@@ -1,20 +1,19 @@
 package com.example.androidappdev.login
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import android.widget.EditText
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.lifecycleScope
 import androidx.lifecycle.repeatOnLifecycle
-import androidx.navigation.fragment.findNavController
 import com.example.androidappdev.R
-import com.example.androidappdev.model.Student
-import kotlinx.coroutines.flow.collect
 import kotlinx.coroutines.launch
 
 class LoginPageFragment: Fragment() {
@@ -31,6 +30,7 @@ class LoginPageFragment: Fragment() {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_login, container, false)
     }
+    @SuppressLint("CutPasteId")
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
@@ -41,21 +41,22 @@ class LoginPageFragment: Fragment() {
         lifecycleScope.launch {
             viewLifecycleOwner.repeatOnLifecycle(Lifecycle.State.STARTED) {
                 viewModel.greetingTextState.collect { greetingTextState ->
-                    view.findViewById<TextView>(R.id.loginPage).text = greetingTextState
+                    view.findViewById<TextView>(R.id.btn_login).text = greetingTextState
                 }
             }
         }
+        val username = view.findViewById<EditText>(R.id.username)
+        val password = view.findViewById<EditText>(R.id.password)
+        val loginButton = view.findViewById<Button>(R.id.btn_login)
 
+        loginButton.setOnClickListener {
+            val user = username.text.toString()
+            val pass = password.text.toString()
+            // Call login method in ViewModel
+        }
 
         //Use viewModel in loginPageFragment
-        println(viewModel::class.java)
-        button = view.findViewById(R.id.login)
-
-        button.setOnClickListener{
-            //Navigate to destination using Fragment Directions
-            val action = LoginPageFragmentDirections.actionLoginPageFragmentToDashboardFragment(username = "Zeba", password = "s4604460", student = Student(username = "Zeba", password = "s4604460"))
-            findNavController().navigate(action)
-        }
+       /* println(viewModel::class.java)*/
 
     }
     /*override fun onViewCreated(view: View, savedInstanceState: Bundle?){
