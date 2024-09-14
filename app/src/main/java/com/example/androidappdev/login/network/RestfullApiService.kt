dcc51da8a958001e71ae9961a52b7250a27fc0e6
ZebaFarhangi <ziba2338@gmail.com>
2024-09-14 13:44:45 +1000

    moved the get to dashboard package

--- app/src/main/java/com/example/androidappdev/login/network/RestfullApiService.kt
+++ app/src/main/java/com/example/androidappdev/login/network/RestfullApiService.kt
@@ -1,8 +1,8 @@
 package com.example.androidappdev.login.network
 
-import com.example.androidappdev.data.DashboardResponse
-import com.example.androidappdev.data.ApiRequest
-import com.example.androidappdev.data.ApiResponse
+import com.example.androidappdev.dashboard.data.DashboardResponse
+import com.example.androidappdev.login.data.ApiRequest
+import com.example.androidappdev.login.data.ApiResponse
 import retrofit2.Response
 import retrofit2.http.Body
 import retrofit2.http.GET
@@ -10,9 +10,6 @@ import retrofit2.http.POST
 import retrofit2.http.Path
 
 interface RestfullApiService {
-    @POST("ort/auth")
+    @POST("/ort/auth")
     suspend fun addObject( @Body data: ApiRequest): ApiResponse
-
-    @GET("dashboard/investments")
-    suspend fun getDashboard(@Path("investments") keypass: String): Response<DashboardResponse>
 }
