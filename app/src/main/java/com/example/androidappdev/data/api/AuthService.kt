8f9a2a654eebcf3aa82d724042823a97aaab7343
ZebaFarhangi <ziba2338@gmail.com>
2024-09-17 23:29:44 +1000

    add SafeArg

--- app/src/main/java/com/example/androidappdev/data/api/AuthService.kt
+++ app/src/main/java/com/example/androidappdev/data/api/AuthService.kt
@@ -5,9 +5,7 @@ import retrofit2.http.POST
 
 interface AuthService {
     @POST("/ort/auth")
-    suspend fun login(
-        @Body credentials: LoginRequest
-    ): LoginResponse
+    suspend fun login(@Body credentials: LoginRequest): LoginResponse
 }
 
 data class LoginRequest(
